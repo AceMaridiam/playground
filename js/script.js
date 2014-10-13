3c933a033f68a681c1749d67df3dbd03daf2da33
Paul Anderson <paul.anderson@metaltoad.com>
2014-10-12 19:04:30 -0700

    added entire playground folder

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/*
+ * Name: script.js
+ * Programmer: Paul Anderson
+ * Date: 8/22/14
+ */
+
+ var container = document.querySelector('#container');
+ var msnry = new Masonry( container, {
+  columnWidth: 200,
+  itemSelector: '.item'
+ });
